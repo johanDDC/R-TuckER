d06dbf0c624de83bd8ce22313ab334d24b9a9e18
johanDDC <ivan,peshekhonov@gmail.com>
2022-04-25 01:33:03 +0300

    Fix metrics computing and predictions filtering

--- utils.py
+++ utils.py
@@ -20,7 +20,6 @@ class R_TuckEROptimizer(Optimizer):
 
     def calc_loss(self, predictions, targets):
         loss = self.model.loss(predictions, targets)
-        # loss.backward()
         return loss
 
     def fit(self, loss_fn, targets):
@@ -66,16 +65,18 @@ def hits_k_metrics(predictions, targets, k = 1):
     return torch.mean(hits)
 
 
-def compute_metrics(predictions, targets, ks):
+def compute_metrics(predictions, targets, ks, accum=None):
     _, idx = torch.sort(predictions, dim=1, descending=True)
     targets_sorted = targets.gather(1, idx)
     ranks = targets_sorted.argmax(1) + 1
-    metrics = dict()
-    metrics["mrr"] = torch.mean(1 / ranks)
+    metrics = dict(mrr=0) if not accum else accum
+    metrics["mrr"] += torch.sum(1 / ranks)
     for k in ks:
+        if not accum:
+            metrics[f"hits_{k}"] = 0
         hits = targets_sorted[:, :k].sum(1).float()
         hits[hits > 1] = 1
-        metrics["hits_" + str(k)] = torch.mean(hits)
+        metrics["hits_" + str(k)] += torch.sum(hits)
     return metrics
 
 
@@ -84,4 +85,6 @@ def filter_predictions(predictions, targets, filter):
     # useful for computing filtered MRR and Hits@k
     interest_prediction_vals = predictions.gather(1, filter)
     predictions[targets == 1] = 0
-    return predictions.scatter_(1, filter, interest_prediction_vals)
+    targets[targets == 1] = 0
+    return predictions.scatter_(1, filter, interest_prediction_vals), \
+            targets.scatter_(1, filter, torch.ones(interest_prediction_vals.shape))
