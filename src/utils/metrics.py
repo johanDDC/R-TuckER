Fix metrics computation

src/utils/metrics.py
@@ -1,19 +1,18 @@
 import torch
-import numpy as np
 
 
-def metrics(prediction, target):
-    zero = torch.tensor([0], device=prediction.device)
-    one = torch.tensor([1], device=prediction.device)
-    target_col = torch.where(target == 1)[1].view(-1, 1)
+def metrics(predictions, targets):
+    _, idx = torch.sort(predictions, dim=1, descending=True)
+    targets_sorted = targets.gather(1, idx)
 
-    indices = prediction.argsort()
-    mrr = (1.0 / (indices == target_col).nonzero()[:, 1].float().add(1.0)).sum().item()
+    ranks = targets_sorted.argmax(dim=1) + 1
+    mrr = torch.sum(1 / ranks).item()
 
     hits = []
     for k in [1, 3, 10]:
-        hits.append(prediction.topk(k=k, largest=False)[1])
-        hits[-1] = torch.where(hits[-1] == target_col, one, zero).float().sum().item()
+        hits_k = targets_sorted[:, :k].sum(dim=1).float()
+        hits_k[hits_k > 1] = 1
+        hits.append(hits_k.sum().item())
 
     return {
         "mrr" : mrr,

src/utils/storage.py
@@ -1,15 +1,16 @@
 import os
 import torch
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
 
-@dataclass
+@dataclass()
 class Losses:
-    train = []
-    test = []
-    val = []
-    norms = []
+    train: List[float] = field(default_factory=list)
+    test: List[float] = field(default_factory=list)
+    val: List[float] = field(default_factory=list)
+    norms: List[float] = field(default_factory=list)
 
     def update(self, train_loss=None, train_norm=None, val_loss=None, test_loss=None):
         self.train.append(train_loss)
@@ -17,22 +18,29 @@ class Losses:
         self.val.append(val_loss)
         self.norms.append(train_norm)
 
+    # def __repr__(self):
+    #     return repr(
+    #         f"Last losses: train {self.train[-1]}, test {self.test[-1]}, val {self.val[-1]}, norm {self.norms[-1]}")
 
-@dataclass
+
+@dataclass()
 class Metric:
-    test = []
-    val = []
+    test: List[float] = field(default_factory=list)
+    val: List[float] = field(default_factory=list)
 
     def __getitem__(self, item):
         return getattr(self, item)
 
+    # def __repr__(self):
+    #     return repr(f"test {self.test[-1]}, val {self.val[-1]}")
 
-@dataclass
+
+@dataclass()
 class Metrics:
-    mrr = Metric()
-    hits_1 = Metric()
-    hits_3 = Metric()
-    hits_10 = Metric()
+    mrr: Metric = field(default_factory=Metric)
+    hits_1: Metric = field(default_factory=Metric)
+    hits_3: Metric = field(default_factory=Metric)
+    hits_10: Metric = field(default_factory=Metric)
 
     def update(self, metrics_dict: dict, type: str):
         self.mrr[type].append(metrics_dict["mrr"])
@@ -40,8 +48,13 @@ class Metrics:
         self.hits_3[type].append(metrics_dict["hits@3"])
         self.hits_10[type].append(metrics_dict["hits@10"])
 
+    # def __repr__(self):
+    #     return repr(f"Last metrics: mrr {repr(self.mrr)}; "
+    #                 f"hits@1 {repr(self.hits_1)}; hits@3 {repr(self.hits_3)}; "
+    #                 f"hits@10 {repr(self.hits_10)}")
 
-@dataclass
+
+@dataclass()
 class StateDict:
     model: dict
     losses: Losses
@@ -62,3 +75,7 @@ class StateDict:
     def load(cls, name):
         state_dict = torch.load(f"{name}.pth")
         return cls(**state_dict)
+
+
+    # def __repr__(self):
+    #     return repr(f"State dict of model {repr(self.model)}")

src/utils/utils.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 
+from src.utils.storage import StateDict
+
 
 def set_random_seed(seed):
     torch.backends.cudnn.deterministic = True
@@ -19,32 +21,37 @@ def filter_predictions(predictions, targets, filter):
            targets.scatter_(1, filter, torch.ones(interest_prediction_vals.shape, device=targets.device))
 
 
-def draw_plots(losses, metrics, baselines=None):
+def draw_plots(state: StateDict, baselines=None):
     import matplotlib.pyplot as plt
     from IPython.display import clear_output
 
     f, ax = plt.subplots(ncols=3, nrows=2, figsize=(24, 9))
     ax[0, 0].set(title="NLL loss", xlabel="Epoches", ylabel="Loss")
-    ax[0, 1].set(title="Mean grad norm on train", xlabel="Epoches", ylabel="Norm")
+    ax[0, 1].set(title="Mean riemann grad norm on train", xlabel="Epoches", ylabel="Norm")
     ax[0, 2].set(title="MRR", xlabel="Epoches", ylabel="MRR")
     ax[1, 0].set(title="Hits@1", xlabel="Epoches", ylabel="Hits@1")
     ax[1, 1].set(title="Hits@3", xlabel="Epoches", ylabel="Hits@3")
     ax[1, 2].set(title="Hits@10", xlabel="Epoches", ylabel="Hits@10")
 
-    epoch = len(metrics["mrr"])
-    x = np.arange(1, epoch + 1)
+    last_epoch = state.last_epoch
+    x = np.arange(1, last_epoch + 1)
     baselines_y = np.ones(len(x))
-    # if len(x) > 1:
-    ax[0, 0].plot(x, losses["train"], c="tab:blue", label="train")
-    ax[0, 0].plot(x, losses["val"], c="tab:green", label="val")
-    ax[0, 0].plot(x, losses["test"], c="tab:orange", label="test")
+    ax[0, 0].plot(x, state.losses.train, c="tab:blue", label="train")
+    ax[0, 0].plot(x, state.losses.val, c="tab:green", label="val")
+    ax[0, 0].plot(x, state.losses.test, c="tab:orange", label="test")
 
-    ax[0, 1].plot(x, losses["norm"], c="tab:red")
-    ax[0, 2].plot(x, metrics["mrr"], c="tab:orange", label="test")
+    ax[0, 1].plot(x, state.losses.norms, c="tab:red")
+    ax[0, 2].plot(x, state.metrics.mrr.test, c="tab:orange", label="test")
+    ax[0, 2].plot(x, state.metrics.mrr.val, c="tab:green", label="val")
 
-    ax[1, 0].plot(x, metrics["hits_1"], c="tab:orange")
-    ax[1, 1].plot(x, metrics["hits_3"], c="tab:orange")
-    ax[1, 2].plot(x, metrics["hits_10"], c="tab:orange")
+    ax[1, 0].plot(x, state.metrics.hits_1.test, c="tab:orange", label="test")
+    ax[1, 0].plot(x, state.metrics.hits_1.val, c="tab:green", label="val")
+
+    ax[1, 1].plot(x, state.metrics.hits_3.test, c="tab:orange", label="test")
+    ax[1, 1].plot(x, state.metrics.hits_3.val, c="tab:green", label="val")
+
+    ax[1, 2].plot(x, state.metrics.hits_10.test, c="tab:orange", label="test")
+    ax[1, 2].plot(x, state.metrics.hits_10.val, c="tab:green", label="val")
 
     if baselines:
         ax[0, 2].plot(x, baselines["mrr"] * baselines_y, color="red", ls="--", lw=1.5, label="baseline")