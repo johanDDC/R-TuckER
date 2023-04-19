1c5fb4b11b41c06e04b3b7f036f92ac709019197
johan <ivan.peshekhonov@gmail.com>
2023-04-20 02:51:15 +0300

    Fix dropout bugs

--- src/utils/layers/Dropout.py
+++ src/utils/layers/Dropout.py
@@ -10,16 +10,15 @@ class _RiemannFactorDropout(torch.autograd.Function):
         # input: B x 2R, where R is corresponding rank
         mask = torch.full((input.shape[0], input.shape[1] // 2), 1 - proba,
                           device=input.device, requires_grad=False)
-        mask = torch.bernoulli(mask)
+        mask = torch.bernoulli(mask) / (1 - proba)
         mask = torch.hstack([mask, torch.ones_like(mask, device=mask.device)])
         ctx.save_for_backward(mask)
-        ctx.proba = proba
-        return (input * mask) / (1 - proba)
+        return input * mask
 
     @staticmethod
     def backward(ctx: Any, grad_output: torch.Tensor):
         mask, = ctx.saved_tensors
-        return (grad_output * mask) / (1 - ctx.proba), None
+        return grad_output * mask, None
 
 
 class RiemannFactorDropout(nn.Module):
