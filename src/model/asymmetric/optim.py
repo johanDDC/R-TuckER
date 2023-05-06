d677b3a69fe9694a48f4e4cac7c58a870b8bc5bc
ivan <ivan.peshekhonov@gmail.com>
2023-05-06 04:59:42 +0300

    Fix momentum for non symmetric optimizer

--- src/model/asymmetric/optim.py
+++ src/model/asymmetric/optim.py
@@ -2,7 +2,7 @@ import torch
 
 from torch.optim import Optimizer
 from tucker_riemopt import Tucker
-from tucker_riemopt.riemopt import compute_gradient_projection, vector_transport
+from tucker_riemopt.riemopt import compute_gradient_projection, vector_transport, group_cores
 
 
 class SGDmomentum(Optimizer):
@@ -28,12 +28,12 @@ class SGDmomentum(Optimizer):
 
     def fit(self, loss_fn, x_k):
         if self.direction:
-            self.momentum = vector_transport(None, x_k, self.direction)
-        riemann_grad = compute_gradient_projection(loss_fn, x_k)
+            self.momentum, _ = vector_transport(None, x_k, self.direction)
+        riemann_grad, self.loss = compute_gradient_projection(loss_fn, x_k)
         grad_norm = riemann_grad.norm()
         riemann_grad = 1 / grad_norm * riemann_grad
         if self.momentum:
-            self.direction = self.momentum_beta * self.momentum + (1 - self.momentum_beta) * riemann_grad
+            self.direction = self._add_momentum(x_k, riemann_grad, self.momentum, self.momentum_beta, 1 / grad_norm)
         else:
             self.direction = riemann_grad
         return grad_norm
@@ -41,7 +41,7 @@ class SGDmomentum(Optimizer):
     def __armijo(self, func, x_k: Tucker, direction: Tucker):
         alpha = 2 * self.lr
         phi_0 = func(x_k)
-        grad_norm = direction.norm(qr_based=False) # set qr_based=True
+        grad_norm = direction.norm(qr_based=False)  # set qr_based=True
         iter_num = 0
 
         retract_alpha = add_and_retract(x_k, direction)
@@ -71,13 +71,29 @@ class SGDmomentum(Optimizer):
         x_k = Tucker(W, [R, S, O])
 
         # self.lr, x_k = self.__armijo(closure, x_k, -self.direction)
-        x_k = add_and_retract(x_k, -self.direction)(self.lr)
+        x_k = add_and_retract(x_k, -self.direction)(self.param_groups[0]["lr"])
+        # x_k = self.add_and_retract_momentum(x_k, -self.direction)(self.param_groups[0]["lr"])
 
         W.data.add_(x_k.core - W)
         R.data.add_(x_k.factors[0] - R)
         S.data.add_(x_k.factors[1] - S)
         O.data.add_(x_k.factors[2] - O)
 
+    def _add_momentum(self, x_k, grad: Tucker, momentum: Tucker, beta: float, grad_norm: float):
+        normalize = 1 / grad_norm
+        dG1 = grad.core[:self.rank[0], :self.rank[1], :self.rank[2]]
+        dR1 = grad.factors[0][:, self.rank[0]:]
+        dS1 = grad.factors[1][:, self.rank[1]:]
+        dO1 = grad.factors[2][:, self.rank[2]:]
+        dG2 = momentum.core[:self.rank[0], :self.rank[1], :self.rank[2]]
+        dR2 = momentum.factors[0][:, self.rank[0]:]
+        dS2 = momentum.factors[1][:, self.rank[1]:]
+        dO2 = momentum.factors[2][:, self.rank[2]:]
+        sum_G = group_cores(normalize * dG1 + beta * dG2, x_k.core)
+        sum_R = torch.hstack([x_k.common_factors[0], normalize * dR1 + beta * dR2])
+        sum_S = torch.hstack([x_k.symmetric_factor, normalize * dS1 + beta * dS2])
+        sum_O = torch.hstack([x_k.symmetric_factor, normalize * dO1 + beta * dO2])
+        return Tucker(sum_G, [sum_R, sum_S, sum_O])
 
 def add_and_retract(x: Tucker, grad: Tucker):
     rank = x.rank
