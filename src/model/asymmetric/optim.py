ba950f071bc9c8f8264ddbc01bce9418931c12f0
johan <ivan.peshekhonov@gmail.com>
2023-07-06 21:14:42 +0300

    Significant rework of optimization methods. Now optimizers fully utilize the logic of tangent vectors from the package

--- src/model/asymmetric/optim.py
+++ src/model/asymmetric/optim.py
@@ -1,63 +1,47 @@
+from typing import Callable, Union
+
 import torch
 
 from torch.optim import Optimizer
 from tucker_riemopt import Tucker
-from tucker_riemopt.riemopt import compute_gradient_projection, vector_transport, group_cores
+from tucker_riemopt import TuckerRiemannian
 
 
-class SGDmomentum(Optimizer):
-    def __init__(self, params, rank, max_lr, momentum_beta=0.9, armijo_slope=1e-4, armijo_increase=0.5,
-                 armijo_decrease=0.5, armijo_iters=20):
+class RSGDwithMomentum(Optimizer):
+    def __init__(self, params, rank, max_lr, momentum_beta=0.9):
         self.rank = rank
         self.max_lr = max_lr
         self.lr = max_lr
         self.momentum_beta = momentum_beta
 
-        self.armijo_slope = armijo_slope
-        self.armijo_increase = armijo_increase
-        self.armijo_decrease = armijo_decrease
-        self.armijo_iters = armijo_iters
-
-        defaults = dict(rank=rank, max_lr=self.max_lr, lr=self.lr, momentum_beta=self.momentum_beta,
-                        armijo_slope=self.armijo_slope, armijo_increase=self.armijo_increase,
-                        armojo_decrease=self.armijo_decrease)
+        defaults = dict(rank=rank, max_lr=self.max_lr, lr=self.lr, momentum_beta=self.momentum_beta)
         super().__init__(params, defaults)
 
         self.momentum = None
         self.direction = None
         self._rank_slices = tuple([slice(0, self.rank[i], None) for i in range(len(self.rank))])
 
-    def fit(self, loss_fn, x_k):
-        if self.direction:
-            self.momentum, _ = vector_transport(None, x_k, self.direction)
-        riemann_grad, self.loss = compute_gradient_projection(loss_fn, x_k)
-        grad_norm = riemann_grad.norm()
-        riemann_grad = 1 / grad_norm * riemann_grad
-        if self.momentum:
-            self.direction = self._add_momentum(x_k, riemann_grad, self.momentum, self.momentum_beta, 1 / grad_norm)
+    def fit(self, loss_fn: Callable[[Tucker], float], x_k: TuckerRiemannian,
+            normalize_grad: Union[float, "False"] = 1.):
+        """
+            Computes the Riemannian gradient of `loss_fn` at point `x_k`.
+
+            :param loss_fn: smooth scalar-valued loss function
+            :param x_k: current solution approximation
+            :param normalize_grad: Can be `False` or float. If `False`, the Riemannian gradient will not be normalized. Otherwise, gradient will
+             be normalized to `normalize_grad`.
+            :return: Frobenius norm of the Riemannian gradient.
+        """
+        if self.direction is not None:
+            self.momentum = TuckerRiemannian.project(x_k, self.direction)
         else:
-            self.direction = riemann_grad
-        return grad_norm
-
-    def __armijo(self, func, x_k: Tucker, direction: Tucker):
-        alpha = 2 * self.lr
-        phi_0 = func(x_k)
-        grad_norm = direction.norm(qr_based=False)  # set qr_based=True
-        iter_num = 0
-
-        retract_alpha = add_and_retract(x_k, direction)
-        new_point = retract_alpha(alpha)
-        last_phi = func(new_point)
-        satisfied = phi_0 - last_phi >= alpha * self.armijo_slope * grad_norm
-
-        while not satisfied and iter_num < self.armijo_iters:
-            alpha *= self.armijo_decrease
-            new_point = retract_alpha(alpha)
-            last_phi = func(new_point)
-            iter_num += 1
-            satisfied = phi_0 - last_phi >= alpha * self.armijo_slope * grad_norm
-
-        return alpha, new_point
+            self.momentum = TuckerRiemannian.TangentVector(x_k, torch.zeros_like(x_k.core))
+        rgrad, self.loss = TuckerRiemannian.grad(loss_fn, x_k)
+        rgrad_norm = rgrad.construct().norm(qr_based=True).detach()
+        normalize_grad = rgrad_norm if not normalize_grad else normalize_grad
+        self.direction = rgrad.linear_comb(1 / rgrad_norm * normalize_grad, self.momentum_beta,
+                                           self.momentum)
+        return rgrad_norm
 
     @torch.no_grad()
     def step(self, closure=None):
@@ -69,45 +53,13 @@ class SGDmomentum(Optimizer):
             A closure that reevaluates the model and returns the loss.
         """
         W, S, R, O = self.param_groups[0]["params"]
-        x_k = Tucker(W, [R, S, O])
 
-        # self.lr, x_k = self.__armijo(closure, x_k, -self.direction)
-        x_k = self._add_and_retract(x_k, -self.direction)(self.param_groups[0]["lr"])
+        x_k = self.direction.linear_comb(-self.param_groups[0]["lr"]).construct()
+        x_k = x_k.round(self.rank)
 
         W.data.add_(x_k.core - W)
         R.data.add_(x_k.factors[0] - R)
         S.data.add_(x_k.factors[1] - S)
         O.data.add_(x_k.factors[2] - O)
 
-    def _add_momentum(self, x_k, grad: Tucker, momentum: Tucker, beta: float, grad_norm: float):
-        normalize = 1 / grad_norm
-        dG1 = grad.core[self._rank_slices]
-        dG2 = momentum.core[self._rank_slices]
-        factors = []
-        for i in range(x_k.ndim):
-            dV1 = grad.factors[i][:, self.rank[i]:]
-            dV2 = momentum.factors[i][:, self.rank[i]:]
-            dV_sum = normalize * dV1 + beta * dV2
-            factors.append(torch.hstack([x_k.common_factors[i], dV_sum]))
-        G_sum = group_cores(normalize * dG1 + beta * dG2, x_k.core)
-        return Tucker(G_sum, factors)
-
-    def _add_and_retract(self, x: Tucker, grad: Tucker):
-        Qs = [None] * x.ndim
-        Rs = [None] * x.ndim
-        for i in range(x.ndim):
-            Qs[i], Rs[i] = torch.linalg.qr(grad.factors[i])
-
-        def f(alpha):
-            temp_core = torch.zeros_like(grad.core, device=grad.core.device)
-            temp_core[self._rank_slices] = x.core
-            sum_core = temp_core + alpha * grad.core
-            inner_tensor = Tucker(sum_core, Rs)
-            inner_tensor = Tucker.full2tuck(inner_tensor.full(), eps=1e-8)
-            factors = [None] * x.ndim
-            for i in range(x.ndim):
-                factors[i] = Qs[i] @ inner_tensor.factors[i]
-                factors[i] = factors[i][:, :self.rank[i]]
-            return Tucker(inner_tensor.core[self._rank_slices], factors)
-
-        return f
+        self.direction = self.direction.construct()
--- src/model/symmetric/optim.py
+++ src/model/symmetric/optim.py
@@ -1,26 +1,20 @@
+from typing import Callable, Union
+
 import torch
 
 from torch.optim import Optimizer
-from tucker_riemopt.symmetric.tucker import Tucker as SymTucker
-from tucker_riemopt.symmetric.riemopt import compute_gradient_projection, vector_transport, group_cores
+from tucker_riemopt import SFTucker
+from tucker_riemopt import SFTuckerRiemannian
 
 
-class SGDmomentum(Optimizer):
-    def __init__(self, params, rank, max_lr, momentum_beta=0.9, armijo_slope=1e-4, armijo_increase=0.5,
-                 armijo_decrease=0.5, armijo_iters=20):
+class RSGDwithMomentum(Optimizer):
+    def __init__(self, params, rank, max_lr, momentum_beta=0.9):
         self.rank = rank
         self.max_lr = max_lr
         self.lr = max_lr
         self.momentum_beta = momentum_beta
 
-        self.armijo_slope = armijo_slope
-        self.armijo_increase = armijo_increase
-        self.armijo_decrease = armijo_decrease
-        self.armijo_iters = armijo_iters
-
-        defaults = dict(rank=rank, max_lr=self.max_lr, lr=self.lr, momentum_beta=self.momentum_beta,
-                        armijo_slope=self.armijo_slope, armijo_increase=self.armijo_increase,
-                        armojo_decrease=self.armijo_decrease)
+        defaults = dict(rank=rank, max_lr=self.max_lr, lr=self.lr, momentum_beta=self.momentum_beta)
         super().__init__(params, defaults)
 
         self.momentum = None
@@ -28,18 +22,27 @@ class SGDmomentum(Optimizer):
         self.loss = None
         self._rank_slices = tuple([slice(0, self.rank[i], None) for i in range(len(self.rank))])
 
-    def fit(self, loss_fn, x_k):
+    def fit(self, loss_fn: Callable[[SFTucker], float], x_k: SFTuckerRiemannian,
+            normalize_grad: Union[float, "False"] = 1.):
+        """
+        Computes the Riemannian gradient of `loss_fn` at point `x_k`.
+
+        :param loss_fn: smooth scalar-valued loss function
+        :param x_k: current solution approximation
+        :param normalize_grad: Can be `False` or float. If `False`, the Riemannian gradient will not be normalized. Otherwise, gradient will
+         be normalized to `normalize_grad`.
+        :return: Frobenius norm of the Riemannian gradient.
+        """
         if self.direction is not None:
-            self.momentum, _ = vector_transport(None, x_k, self.direction)
-        riemann_grad, self.loss = compute_gradient_projection(loss_fn, x_k)
-        grad_norm = riemann_grad.norm(qr_based=True).detach()
-        if self.momentum is not None:
-            self.direction = self._add_momentum(x_k, riemann_grad, self.momentum,
-                                                self.momentum_beta, grad_norm)
+            self.momentum = SFTuckerRiemannian.project(x_k, self.direction)
         else:
-            riemann_grad = 1 / grad_norm * riemann_grad
-            self.direction = riemann_grad
-        return grad_norm
+            self.momentum = SFTuckerRiemannian.TangentVector(x_k, torch.zeros_like(x_k.core))
+        rgrad, self.loss = SFTuckerRiemannian.grad(loss_fn, x_k)
+        rgrad_norm = rgrad.construct().norm(qr_based=True).detach()
+        normalize_grad = rgrad_norm if not normalize_grad else normalize_grad
+        self.direction = rgrad.linear_comb(1 / rgrad_norm * normalize_grad, self.momentum_beta,
+                                           self.momentum)
+        return rgrad_norm
 
     @torch.no_grad()
     def step(self, closure=None):
@@ -51,34 +54,12 @@ class SGDmomentum(Optimizer):
             A closure that reevaluates the model and returns the loss.
         """
         W, E, R = self.param_groups[0]["params"]
-        x_k = SymTucker(W, [R], num_symmetric_modes=2, symmetric_factor=E)
 
-        # self.lr, x_k = self.__armijo(closure, x_k, -self.direction)
-        x_k = self._add(x_k, -self.direction, self.param_groups[0]["lr"])
+        x_k = self.direction.linear_comb(-self.param_groups[0]["lr"]).construct()
         x_k = x_k.round(self.rank)
 
         W.data.add_(x_k.core - W)
-        R.data.add_(x_k.common_factors[0] - R)
-        E.data.add_(x_k.symmetric_factor - E)
+        R.data.add_(x_k.regular_factors[0] - R)
+        E.data.add_(x_k.shared_factor - E)
 
-    def _add_momentum(self, x_k: SymTucker, grad: SymTucker, momentum: SymTucker, beta: float, grad_norm: float):
-        normalize = 1 / grad_norm
-        dG1 = grad.core[self._rank_slices]
-        dG2 = momentum.core[self._rank_slices]
-        V_sum = []
-        for i in range(x_k.ndim - x_k.num_symmetric_modes):
-            dV1 = grad.common_factors[i][:, self.rank[i]:]
-            dV2 = momentum.common_factors[i][:, self.rank[i]:]
-            dV_sum = normalize * dV1 + beta * dV2
-            V_sum.append(torch.hstack([x_k.common_factors[i], dV_sum]))
-        dU1 = grad.symmetric_factor[:, self.rank[-1]:]
-        dU2 = momentum.symmetric_factor[:, self.rank[-1]:]
-        U_sum = torch.hstack([x_k.symmetric_factor, normalize * dU1 + beta * dU2])
-        G_sum = group_cores(normalize * dG1 + beta * dG2, x_k.core)
-        return SymTucker(G_sum, V_sum, 2, U_sum)
-
-    def _add(self, x: SymTucker, direction: SymTucker, alpha):
-        temp_core = torch.zeros_like(direction.core, device=direction.core.device)
-        temp_core[self._rank_slices] = x.core
-        sum_core = temp_core + alpha * direction.core
-        return SymTucker(sum_core, direction.common_factors, direction.num_symmetric_modes, direction.symmetric_factor)
+        self.direction = self.direction.construct()
